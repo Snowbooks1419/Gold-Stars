e2230fdd953163c768f99d9ae99aadae4f09de01
SirFaizdat <sirfaizdat@gmail.com>
2015-03-14 17:50:33 -0400

    GoldStars Browser test.

--- src/goldstars/GoldStars.java
+++ src/goldstars/GoldStars.java
@@ -1,7 +1,68 @@
 package goldstars;
 
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.beans.value.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.web.*;
+import javafx.stage.Stage;
 
-public class GoldStars extends JFrame {
+public class GoldStars extends Application {
+    public static final String DEFAULT_URL = "http://www.google.com/";
+
+    private void init(Stage primaryStage) {
+        primaryStage.setTitle("GoldStars Browser");
+        Group root = new Group();
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        WebView webView = new WebView();
+        webView.setPrefSize(1280, 720);
+
+        final WebEngine webEngine = webView.getEngine();
+        webEngine.load(DEFAULT_URL);
+
+        final TextField locationField = new TextField(DEFAULT_URL);
+        webEngine.locationProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                locationField.setText(newValue);
+            }
+        });
+        EventHandler<ActionEvent> goAction = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                webEngine.load(locationField.getText().startsWith("http://")
+                        ? locationField.getText()
+                        : "http://" + locationField.getText());
+            }
+        };
+        locationField.setOnAction(goAction);
+
+        Button goButton = new Button("Go");
+        goButton.setDefaultButton(true);
+        goButton.setOnAction(goAction);
+
+        // Layout logic
+        HBox hBox = new HBox(5);
+        hBox.getChildren().setAll(locationField, goButton);
+        HBox.setHgrow(locationField, Priority.ALWAYS);
+
+        VBox vBox = new VBox(5);
+        vBox.getChildren().setAll(hBox, webView);
+        VBox.setVgrow(webView, Priority.ALWAYS);
+
+        root.getChildren().add(vBox);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        init(primaryStage);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
